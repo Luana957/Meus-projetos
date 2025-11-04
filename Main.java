16de97a45bb1ac671d4b7d5c331dd0f3bb495a5e
Luana957 <luanaestefany38@gmail.com>
2025-11-04 16:18:32 -0300

    add cadastro

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Luana Estéfany";
+        int idade = 20;
+        String cpf = "123.456.789-00";
+        String email = "luana@email.com";
+        int matricula = 101;
+
+        System.out.println("=== Dados do Cadastro ===");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Email: " + email);
+        System.out.println("Matrícula: " + matricula);
+    }
+}
