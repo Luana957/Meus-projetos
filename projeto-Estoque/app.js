00733e6c02d63dc61a5d6b592a5cd1d4fb40b35a
Luana957 <luanaestefany38@gmail.com>
2026-05-01 17:09:03 -0300

    "Configurando servidor Node e gitignore"

--- projeto-Estoque/app.js
+++ projeto-Estoque/app.js
@@ -1,1 +1,16 @@
- 
+ const express = require('express');
+const app = express();
+const path = require('path');
+const porta = 3000;
+
+// Configura o Node para servir seus arquivos (HTML, CSS, imagens)
+app.use(express.static(__dirname));
+
+// Rota principal que entrega o seu index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(porta, () => {
+    console.log(`✅ Servidor bombando em http://localhost:${porta}`);
+});
